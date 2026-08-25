149482b5f0f8826a80238c1629700666b532a85a
khdjkbr <140163401+khdjkbr@users.noreply.github.com>
2026-08-26 02:28:53 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
+CHANNEL_USERNAME = "@unbox_uzb"
+CHANNEL_URL = "https://t.me/unbox_uzb"
+PROMO_CAPTION = "📥 @videoni_yuklaydigan_bot orqali yuklab olindi!\n📢 Yanada qiziqarli ma'lumotlar @unbox_uzb kanalida, obuna bo'ling!"
+PORT = int(os.getenv("PORT", 10000))
